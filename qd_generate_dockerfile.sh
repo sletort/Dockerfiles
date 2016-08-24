faec715ea974dbcf2a0efa04d53fc7b52a6bf7b4
Sébastien Letort <sebastien.letort@irisa.fr>
2016-08-24 11:21:58 +0200

    Update qd_generate_dockerfile.sh
    previous commit was unexpected.
    This one provides the full *untested* script.

--- qd_generate_dockerfile.sh
+++ qd_generate_dockerfile.sh
@@ -1,31 +1,44 @@
 #! /bin/bash
 
 ## AIMS  : Generate a dockerfile based on theDockerfile.template
-## USAGE :
-## NOTE  :
+## USAGE : qd_generate_dockerfile.sh prog version
+## NOTE  : Dockerfile still need to be completed.
+## NOTE  : if Dockerfile already exists, it will be moved to a .bk file.
 ## AUTHORS : sletort@irisa.fr
 
 set -o nounset
 set -o errexit
 
 # =========================================================
-declare -r INFILE="${1?What is your input file ?}"
+declare -r PROG="${1?What is your program name ?}"
+declare -r VERSION="${1?What is the version of the program ?}"
 
 # ---------------------------------------------------------
-#declare -r MG_MAP_DIR="/PUBLIC_DATA/GENERATED/1kG/map_files"
 
 # =========================================================
 function printVars ()
 {
 	printf "╔═══════════════════════════════════ ═ ═ ═ ═\n"
-	printf "╟ - SCRIPT     = $0\n"
-	printf "╟ - INFILE     = $INFILE\n"
+	printf "╟ - SCRIPT    = $0\n"
+	printf "╟ - PROG      = $PROG\n"
+	printf "╟ - VERSION   = $VERSION\n"
 	printf "╚═══════════════════════════════════ ═ ═ ═ ═\n\n"
 }
 
 # =========================================================
 function main ()
 {
+	local -r OUTDIR="${PROG}/${VERSION}"
+	mkdir -p "$OUTDIR"
+
+	if [[ -e "$OUTDIR"/Dockerfile ]]
+	then
+		cp "$OUTDIR"/Dockerfile "$OUTDIR"/Dockerfile.bk
+	fi
+
+	local perle='s/XXXXX/'$PROG'/g;'
+	perle=$perle' s/VVVV/'$VERSION'/g;'
+	perl -pe "$perle" Dockerfile.template > "$OUTDIR"/Dockerfile
 }
 
 # =========================================================
@@ -34,6 +47,6 @@ printVars
 # your work.
 main
 
-printf "End of %s.\n\n" $( basename $0 )
+#printf "End of %s.\n\n" $( basename $0 )
 
 exit 0
